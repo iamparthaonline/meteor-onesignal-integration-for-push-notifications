036f1e0c5884885e1d05cb4c8035fb7161a79e0a
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2022-10-06 20:53:15 +0530

    Initialized with push notification sdk + set the app + permission

--- client/main.jsx
+++ client/main.jsx
@@ -5,4 +5,19 @@ import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
   render(<App/>, document.getElementById('react-target'));
+
+  if(Meteor.isCordova){
+
+    function initializeOnesignalPushSDK(){
+      alert('opened in mobile');
+      window.plugins.OneSignal.setAppId('2722650b-a374-48bd-9e8f-c4446e87895e');
+  
+      window.plugins.OneSignal.promptForPushNotificationsWithUserResponse( function(accepted) {
+        alert("User has accepted push notifications", accepted );
+      })
+    }
+
+    document.addEventListener('deviceready', initializeOnesignalPushSDK, false);
+    
+  }
 });
